f0f11143db58d2437b45c7c9ab5e1027affecc8b
FajarOfficial199 <fajarshidik709@gmail.com>
2025-03-07 23:55:56 +0700

    Update nsfw-hneko.js

--- src/api/nsfw/nsfw-hneko.js
+++ src/api/nsfw/nsfw-hneko.js
@@ -1,16 +1,20 @@
 const axios = require('axios');
 
-module.exports = function(app) {
-app.get('/nsfw/hentai-neko', async (req, res) => {
-    try {
-        const neko = await axios.get('https://waifu.pics/api/nsfw/neko');
-        res.writeHead(200, {
+module.exports = function (app) {
+    app.get('/nsfw/hentai-neko', async (req, res) => {
+        try {
+            const response = await axios.get('https://waifu.pics/api/nsfw/neko');
+            const imageUrl = response.data.url;
+
+            const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+
+            res.writeHead(200, {
                 'Content-Type': 'image/png',
-                'Content-Length': neko.length,
+                'Content-Length': imageResponse.data.length,
             });
-            res.end(neko);
-    } catch (error) {
-        res.status(500).json({ error: 'Failed to fetch NSFW image' });
-    }
-});
-}
+            res.end(imageResponse.data);
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to fetch NSFW image' });
+        }
+    });
+};
