ce0a3e5d0441e36c38ff652714a686cabc65f3d1
FajarOfficial199 <fajarshidik709@gmail.com>
2025-03-10 20:09:57 +0700

    Update downloader-sfile.js

--- src/api/downloader/downloader-sfile.js
+++ src/api/downloader/downloader-sfile.js
@@ -1,5 +1,3 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
 const vioo = require('@vioo/apis')
 
 module.exports = function (app) {
@@ -15,7 +13,7 @@ app.get('/downloader/sfile', async (req, res) => {
   }
 
   try {
-    const result = await vioo.download.sfileDl(url);
+    const result = await vioo.downloader.sfileDl(url);
 
     if (!result.dl) {
       return res.status(404).json({ error: "Link download tidak tersedia", details: result });
