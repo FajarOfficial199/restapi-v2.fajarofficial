2282d7006657756defb1a8d6170a9131bdf6355a
FajarOfficial199 <fajarshidik709@gmail.com>
2026-06-07 00:33:07 +0700

    Refactor profile endpoint to use POST method

--- src/api/topuku/profile.js
+++ src/api/topuku/profile.js
@@ -1,28 +1,32 @@
-const express = require("express");
 const axios = require("axios");
 const crypto = require("crypto");
-const cors = require("cors");
-module.exports = function (app) {
-  function md5(text) {
-    return crypto.createHash("md5").update(text).digest("hex");
-}
-app.get("/api/topupku/profile", async (req, res) => {
-    try {
-      const { api_id, api_key } = req.body
-        const response = await axios.post(
-            "https://topupku.com/api/profile",
-            {
-                api_id: api_id,
-                api_key: api_key,
-                signature: md5(API_ID + API_KEY)
-            }
-        );
 
-        res.json(response.data);
-    } catch (err) {
-        res.status(500).json({
-            error: err.message
-        });
+module.exports = function (app) {
+
+    function md5(text) {
+        return crypto.createHash("md5").update(text).digest("hex");
     }
-});
-}
+
+    app.post("/api/topupku/profile", async (req, res) => {
+        try {
+            const { api_id, api_key } = req.body;
+
+            const response = await axios.post(
+                "https://topupku.com/api/profile",
+                {
+                    api_id,
+                    api_key,
+                    signature: md5(api_id + api_key)
+                }
+            );
+
+            res.json(response.data);
+
+        } catch (err) {
+            res.status(500).json({
+                error: err.response?.data || err.message
+            });
+        }
+    });
+
+};
