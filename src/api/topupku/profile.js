e461c3ebc1a387da4f78b38390bd5a687a296462
FajarOfficial199 <fajarshidik709@gmail.com>
2026-06-07 00:35:28 +0700

    Create profile.js

---
+++ src/api/topupku/profile.js
@@ -0,0 +1,32 @@
+const axios = require("axios");
+const crypto = require("crypto");
+
+module.exports = function (app) {
+
+    function md5(text) {
+        return crypto.createHash("md5").update(text).digest("hex");
+    }
+
+    app.get("/api/topupku/profile", async (req, res) => {
+        try {
+            const { api_id, api_key } = req.body;
+
+            const response = await axios.post(
+                "https://topupku.com/api/profile",
+                {
+                    api_id,
+                    api_key,
+                    signature: md5(api_id + api_key)
+                }
+            );
+
+            res.json(response.data);
+
+        } catch (err) {
+            res.status(500).json({
+                error: err.response?.data || err.message
+            });
+        }
+    });
+
+};
